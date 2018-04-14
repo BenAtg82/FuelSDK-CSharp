2e2431fcfddf6e0351bfc8582520625a3eb05261
pepperpark <steve@rakebrandt.com>
2018-04-14 23:24:14 +0100

    Ported to netstandard2.0

--- FuelSDK-CSharp/ETExtractDescription.cs
+++ FuelSDK-CSharp/ETExtractDescription.cs
@@ -5,12 +5,12 @@ using System.Text;
 
 namespace FuelSDK
 {
-    public class ETExtractDescription : ExtractDescription
-    {
-        /// <summary>
-        /// Get this instance.
-        /// </summary>
-        /// <returns>The <see cref="T:FuelSDK.GetReturn"/> object..</returns>
-        public GetReturn Get() { var r = new GetReturn(this); LastRequestID = r.RequestID; return r; }
-    }
+	public class ETExtractDescription : ExtractDescription
+	{
+		/// <summary>
+		/// Get this instance.
+		/// </summary>
+		/// <returns>The <see cref="T:FuelSDK.GetReturn"/> object..</returns>
+		public GetReturn Get() { var r = new GetReturn(this); LastRequestID = r.RequestID; return r; }
+	}
 }
